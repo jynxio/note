3bcd4ba14d6acd4e3b0b1db75e954ea329a1b7a8
Matrix <1337816495@qq.com>
2021-12-24 23:33:04 +0800

    add 《entry》

---
+++ babel/示例代码/填补接口/按需填补/entry/chrome85.js
@@ -0,0 +1,23 @@
+"use strict";
+
+require("core-js/modules/es.error.cause.js");
+
+require("core-js/modules/es.aggregate-error.cause.js");
+
+require("core-js/modules/es.array.at.js");
+
+require("core-js/modules/es.object.has-own.js");
+
+require("core-js/modules/es.reflect.to-string-tag.js");
+
+require("core-js/modules/es.string.at-alternative.js");
+
+require("core-js/modules/es.typed-array.at.js");
+
+require("core-js/modules/web.dom-exception.stack.js");
+
+require("core-js/modules/web.immediate.js");
+
+require("core-js/modules/web.structured-clone.js");
+
+require("regenerator-runtime");
---
+++ babel/示例代码/填补接口/按需填补/entry/chrome95.js
@@ -0,0 +1,9 @@
+"use strict";
+
+require("core-js/modules/web.dom-exception.stack.js");
+
+require("core-js/modules/web.immediate.js");
+
+require("core-js/modules/web.structured-clone.js");
+
+require("regenerator-runtime");
