687346d555e07c6ea263ba19b7f61674f02e484d
Matrix <1337816495@qq.com>
2022-01-03 03:15:44 +0800

    u

--- webpack/示例代码/管理输出/src/index.js
+++ webpack/示例代码/管理输出/src/index.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
 
-
+import printMe from "./print.js";
 
 function component() {
 
     const element = document.createElement("div");
     element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+    const button = document.createElement("button");
+    button.innerHTML = "Click me and check the console!";
+    button.onclick = printMe;
+
+    element.appendChild(button);
 
     return element;
 
--- webpack/示例代码/管理输出/webpack.config.js
+++ webpack/示例代码/管理输出/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require("path");
 
+const html_wepack_plugin = require("html-webpack-plugin");
+
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
+        clean: true,
     },
+    plugins: [
+        new html_wepack_plugin({
+            title: "管理输出"
+        }),
+    ],
 };
