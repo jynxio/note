fc91ebaeea62caf07c738f160b0d8536261b3b3e
X <1337816495@qq.com>
2022-01-05 14:55:07 +0800

    增加CSS压缩

--- webpack/我的环境/bundler/webpack.dev.js
+++ webpack/我的环境/bundler/webpack.dev.js
@@ -1,15 +1,15 @@
-const { merge } = require( "webpack-merge" );
+const { merge } = require("webpack-merge");
 
-const common = require( "./webpack.common.js" );
+const common = require("./webpack.common.js");
 
-module.exports = merge( common, {
+module.exports = merge(common, {
     mode: "development",
-    devtool: "eval-cheap-module-source-map",
+    devtool: "eval-cheap-module-source-map", // 激活源码映射：它既能提供质量较好的源码映射，又能提供较高的构建速度。
     devServer: {
-        static: "./",
-        compress: true,
-        server: "http",
-        port: 8080,
-        open: true,
+        static: "./",                        // 设置资源的起寻地址。
+        compress: true,                      // 激活gzip。
+        server: "http",                      // 设置网络传输协议。
+        port: 8080,                          // 设置端口号。
+        open: true,                          // 激活浏览器自启动。
     },
-} );
+});
